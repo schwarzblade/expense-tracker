59dc2bd25783e39c359ae045587526df772ae623
luk <schwarzblade@gamil.com>
2020-04-06 14:48:46 +0200

    delete action

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,19 +14,25 @@ const initialState = {
 }
 
 //create context
-
 export const GlobalContext = createContext(initialState);
 
 
 //Provider component
-
-
 export const GlobalProvider = ({children}) => {
 	const [state,dispatch] = useReducer(AppReducer, initialState);
 
 
+//Actions
+function deleteTransaction(id){
+	dispatch({
+		type: 'DELETE_TRANSACTION',
+		payload: id
+	});
+}
+
 	return(<GlobalContext.Provider value={{
-		transactions: state.transactions
+		transactions: state.transactions,
+		deleteTransaction
 	}}>
 		{children}
 	</GlobalContext.Provider>);
